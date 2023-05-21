f567448c92417654cb6efbe984389b6ab8990b3f
ekye0512 <38061754+ekye0512@users.noreply.github.com>
2023-05-20 17:45:12 -0700

    Revert "Backend setup"
    This reverts commit 424f88b922a59faa4eb21e5eeac299aa86f9a351.

--- server/routes/auth.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { login } from "../controllers/auth.js";
-
-const router = express.Router(); // sets up a router
-
-router.post("/login", login); // will be /auth/login bc of the route
-
-export default router;
